e1247add70775302cd317a9ba4a7c563072d8bc0
kiranraoP <32511577+kiranraoP@users.noreply.github.com>
2017-10-04 16:32:20 +0530

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("DBASE_NAME", "mystery");
-define("DBASE_HOST", "localhost");
-define("DBASE_USER", "root");
-define("DBASE_PWD", "");
-
-define("BASE_PATH", "http://localhost/projects/M/Mystery_lunch/");
-date_default_timezone_set('Asia/Kuwait');
-?>
