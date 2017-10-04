d37705e6743ac73bde328819a5eb80fd84b07d24
kiranraoP <32511577+kiranraoP@users.noreply.github.com>
2017-10-04 15:56:18 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/*define("DBASE_NAME", "soccer_new");
+define("DBASE_HOST", "localhost");
+define("DBASE_USER", "soccer_new");
+define("DBASE_PWD", "hJ2.;9c#02+m");*/
+define("DBASE_NAME", "mystery");
+define("DBASE_HOST", "localhost");
+define("DBASE_USER", "root");
+define("DBASE_PWD", "");
+
+define("BASE_PATH", "http://localhost/projects/M/Mystery_lunch/");
+date_default_timezone_set('Asia/Kuwait');
+?>
